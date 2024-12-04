823819dcfc12d1f08d6a0e5923c7fc95964b64d7
Tiago Alves <tiagojba9@ua.pt>
2024-12-04 00:53:07 +0000

    restructured vehicle dynamics and started perception from remote broker

--- broker-to-ditto/daemon.py
+++ broker-to-ditto/daemon.py
@@ -2,8 +2,24 @@
 import paho.mqtt.client as mqtt
 import json
 import requests
-from pygeotile.tile import Tile
 import threading
+import tiles
+import time
+import math
+
+class bcolors:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    RED = '\033[93m'
+    ENDC = '\033[0m'
+
+    @classmethod
+    def log_warning_red(cls, message):
+        print(cls.RED +message +cls.ENDC)
+    @classmethod
+    def log_warning_blue(cls,message):
+        print(cls.CYAN +message +cls.ENDC)
 
 BROKER_HOST = "es-broker.av.it.pt"
 BROKER_PORT = 8090
@@ -16,63 +32,223 @@ DITTO_THING_ID = "org.acme:my-device-2" # TODO: This can be dynamic (getting the
 DITTO_USERNAME = 'ditto'
 DITTO_PASSWORD = 'ditto'
 
-mqtt_client = None
-current_topic = None
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, transport='websockets')
+current_original_topic = "placeholder"
+current_subscribed_topics = set()
+local_map = []
 
-def update_ditto(payload):
-    url = f"{DITTO_BASE_URL}/{DITTO_THING_ID}/features/remote-broker"
+# Clear the local perception map every 10 seconds
+def clear_map():
+    while (1):
+        local_map.clear()
+        time.sleep(10)
+
+def create_json(local_map):
+    objects_json = {
+        "properties": {}
+    }
+
+    for obj in local_map:
+        obj_id, obj_lat, obj_lon = obj
+        objects_json["properties"][obj_id] = {
+            "latitude": obj_lat,
+            "longitude": obj_lon
+        }
+
+    return objects_json
+
+def manage_ditto_perception():
+    while (1):
+        perception = create_json(local_map)
+        update_ditto_perception(perception)
+        time.sleep(0.1) 
+        
+def cpm_to_local_map(payload):
+    payload = json.loads(payload)
+
+    station_position = [
+        payload["cpm"]["cpmParameters"]["managementContainer"]["referencePosition"]["latitude"] / 10e6,
+        payload["cpm"]["cpmParameters"]["managementContainer"]["referencePosition"]["longitude"] / 10e6
+    ]
+
+    perceived_objects = payload["cpm"]["cpmParameters"].get("perceivedObjectContainer")
+    if perceived_objects:
+        # Calculate the position of the perceived objects and add them to local map
+        for obj in perceived_objects:
+            obj_id = obj["objectID"]
+            obj_lat = station_position[0] + (obj["yDistance"]["value"] / 6371000 / 100) * (180 / math.pi) 
+            obj_lon = station_position[1] + ((obj["xDistance"]["value"] / 6371000 / 100) * (180 / math.pi)) / math.cos(((station_position[0] + (obj["yDistance"]["value"] / 6371000 / 100) * (180 / math.pi)) * math.pi / 180))
+            local_map.append((obj_id, obj_lat, obj_lon))
+
+
+def update_ditto_perception(local_map):
+    url = f"{DITTO_BASE_URL}/{DITTO_THING_ID}/features/Perception"
     # Set up headers and basic authentication
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     
-    # Send HTTP PUT request to update the twin in Ditto
-    response = requests.put(url, headers=headers, auth=(DITTO_USERNAME, DITTO_PASSWORD), json=payload)
-    if response.status_code in (201, 204):
-        print("Ditto twin updated successfully.")
+    # Send HTTP PUT request to update the twin in Ditto; TODO: Verify status code of response if needed
+    requests.put(url, headers=headers, auth=(DITTO_USERNAME, DITTO_PASSWORD), json=local_map)
+
+
+def update_ditto_dynamics(dynamics):
+    url = f"{DITTO_BASE_URL}/{DITTO_THING_ID}/features/Dynamics"
+    # Set up headers and basic authentication
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    
+    # Send HTTP PUT request to update the twin in Ditto; TODO: Verify status code of response if needed
+    requests.put(url, headers=headers, auth=(DITTO_USERNAME, DITTO_PASSWORD), json=dynamics)
+
+
+def obtain_dynamics(payload):
+    payload = json.loads(payload)
+
+    device_id = payload["header"]["stationId"]
+
+    cam = payload["cam"]["camParameters"]
+    timestamp = payload["cam"]["generationDeltaTime"]
+
+    ### Basic container ###
+    sta_type = cam["basicContainer"]["stationType"]
+    ref_pos = cam["basicContainer"]["referencePosition"]
+    
+    ### HF container ###
+    high_freq_container = cam["highFrequencyContainer"]["basicVehicleContainerHighFrequency"]
+
+    heading = high_freq_container["heading"]
+    speed = high_freq_container["speed"]
+    drive_direction = high_freq_container["driveDirection"]
+    long_accel = high_freq_container["longitudinalAcceleration"]
+    curv = high_freq_container["curvature"]
+    curv_calc_mode = high_freq_container["curvatureCalculationMode"]
+    yaw_rate = high_freq_container["yawRate"]
+    
+    ### LF container ###
+    if "lowFrequencyContainer" in cam:
+        low_freq_container = cam["lowFrequencyContainer"]["basicVehicleContainerLowFrequency"]
+
+        vehicle_role = low_freq_container["vehicleRole"]
+        ext_lights = low_freq_container["exteriorLights"]
+        path_hist = low_freq_container["pathHistory"]
+
+        dynamics = {
+            "properties": {
+                "generationDeltaTime": timestamp,
+                "basicContainer": {
+                    "stationType": sta_type,
+                    "referencePosition": ref_pos
+                },
+                "highFrequencyContainer": {
+                    "heading": heading,
+                    "speed": speed,
+                    "driveDirection": drive_direction,
+                    "longitudinalAcceleration": long_accel,
+                    "curvature": curv,
+                    "curvatureCalculationMode": curv_calc_mode,
+                    "yawRate": yaw_rate
+                },
+                "lowFrequencyContainer": {
+                    "vehicleRole": vehicle_role,
+                    "exteriorLights": ext_lights,
+                    "pathHistory": path_hist
+                }
+            }
+        }
+    # If there is no LF container, just don't include it in the dynamics
     else:
-        print(f"Failed to update Ditto twin. Status code: {response.status_code}")
-        print("Response:", response.text)
+        dynamics = {
+            "properties": {
+                "generationDeltaTime": timestamp,
+                "basicContainer": {
+                    "stationType": sta_type,
+                    "referencePosition": ref_pos
+                },
+                "highFrequencyContainer": {
+                    "heading": heading,
+                    "speed": speed,
+                    "driveDirection": drive_direction,
+                    "longitudinalAcceleration": long_accel,
+                    "curvature": curv,
+                    "curvatureCalculationMode": curv_calc_mode,
+                    "yawRate": yaw_rate
+                }
+            }
+        }
+
+    update_ditto_dynamics(dynamics)
 
 
-def on_area_message(client, userdata, message):
-    payload_json = json.loads(message.payload)
+def manage_current_tile(message):
+    # TODO: Testing and verification
+    global current_original_topic, current_subscribed_topics
 
-    # Check if the message is coming from the own vehicle
     topic = message.topic.split("/")
-    print("\ntopic[3]: \n" + topic[3])
-    if topic[3] == "20":
-        quadtree_map = '/'.join(topic[5:])
-        print("\n\nExtracted quadtree map: " + quadtree_map + "\n\n")
-        new_topic = f"its_center/inqueue/json/+/+/{quadtree_map}"
-        if new_topic != current_topic:
-            print(f"Switching subscription to new quadtree topic: {new_topic}")
-            subscribe_to_new_area(new_topic)
-        return  # TODO remove if want to update DITTO with own vehicle data (20)
+    original_topic_tile = '/'.join(topic[5:])
+    original_topic_path = f"its_center/inqueue/json/+/+/{original_topic_tile}"
+   
+    # If vehicle did not change tile, don't need to modify subscriptions
+    if (original_topic_path == current_original_topic):
+        return
 
-    print("\n\nReceived message '" + str(payload_json) + "' on topic '" + message.topic + "'\n\n")
-    ditto_payload = {
-        "properties": payload_json
-    }
+    bcolors.log_warning_blue(f"Switching original subscription to new quadtree topic: {original_topic_path}")
 
-    update_ditto(ditto_payload)
+    it2s_tiles = tiles.It2s_Tiles()
+    # Get the adjacent tiles (in all directions) NOTE: The original tile is also included, by calculation of Hold direction
+    adjacent_tiles = set(it2s_tiles.it2s_get_all_adjacent_tiles(original_topic_tile))
+
+    new_subscribed_topics = set()
+
+    for tile in adjacent_tiles:
+        tile_topic = f"its_center/inqueue/json/+/+/{tile}"
+        new_subscribed_topics.add(tile_topic)
+    
+    topics_to_unsubscribe = current_subscribed_topics - new_subscribed_topics
+    topics_to_subscribe = new_subscribed_topics - current_subscribed_topics
+    
+    for topic in topics_to_unsubscribe:
+        #bcolors.log_warning_red(f"Unsubscribing from topic: {topic}")
+        mqtt_client.unsubscribe(topic)
+
+    for topic in topics_to_subscribe:
+        #bcolors.log_warning_red(f"Subscribing to new topic: {topic}")
+        mqtt_client.subscribe(topic)
+
+    current_original_topic = original_topic_path
+    current_subscribed_topics = new_subscribed_topics
+    for topic in current_subscribed_topics:
+        bcolors.log_warning_red(f"Subscribed to topic: {topic}")
 
 
-def subscribe_to_new_area(new_topic):
-    global current_topic, mqtt_client
-    if current_topic:
-        mqtt_client.unsubscribe(current_topic)
-    mqtt_client.subscribe(new_topic)
-    current_topic = new_topic
+def on_message_cb(client, userdata, message):
+    #print("Received message on topic '" + message.topic)
+    station_id = message.topic.split("/")[3]
+    
+    if (station_id == "20"):
+        #print(bcolors.CYAN + "Received message on topic '" + message.topic + bcolors.ENDC)
+        manage_current_tile(message)
+        
+        if ("CAM" in message.topic):
+            obtain_dynamics(message.payload)
+
+    elif (station_id != 20):
+        if ("CPM" in message.topic):
+            cpm_to_local_map(message.payload)
+    #     elif ("CAM" in message.topic):
+    #         cam_to_local_map(message.payload)
+
+
+def on_connect_cb(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print("failed to connect")
+
+    print("Subscribing to topic: " + MQTT_INITIAL_TOPIC)
+    mqtt_client.subscribe(MQTT_INITIAL_TOPIC)
 
 
 def setup_initial_mqtt():
-    global mqtt_client
-
-    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2,transport='websockets')
     mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-    mqtt_client.on_message = on_area_message
+    mqtt_client.on_message = on_message_cb
+    mqtt_client.on_connect = on_connect_cb
     mqtt_client.connect(BROKER_HOST, BROKER_PORT)
-    print("Subscribing to topic: " + MQTT_INITIAL_TOPIC)
-    mqtt_client.subscribe(MQTT_INITIAL_TOPIC)
 
     return mqtt_client
 
@@ -83,9 +259,15 @@ def subscribe_to_get_vehicle_area():
     client.loop_forever()
 
 
-def run_daemon():
-    initial_thread = threading.Thread(target=subscribe_to_get_vehicle_area)
+def start_agent_threads():
+    initial_thread = threading.Thread(target=subscribe_to_get_vehicle_area, daemon=True)
     initial_thread.start()
+
+    perception_thread = threading.Thread(target=manage_ditto_perception, daemon=True)
+    perception_thread.start()
+
+    clear_map_thread = threading.Thread(target=clear_map, daemon=True)
+    clear_map_thread.start()
 
     try:
         while True:
@@ -94,5 +276,7 @@ def run_daemon():
         print("Broker-to-ditto daemon interrupted. Shutting down...")
         mqtt_client.disconnect()
 
+
+
 if __name__ == "__main__":
-    run_daemon()
+    start_agent_threads()
